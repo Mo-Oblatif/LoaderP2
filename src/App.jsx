6a678c8ab9255154ba4204d4765fb2e917d226ab
Mo-Oblatif <martinezmoana@gmail.com>
2021-08-30 17:01:51 +0200

    loader timeout app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import './App.css';
 import Loader from './components/Loader'
 
 function App() {
+  //  const [loader, setLoader] = useState(true);
+
+  // useEffect(() => {
+    
+  //   setTimeout (() => {
+  //     setLoader(false);
+  //   }, 2500)
+
+  // }, [])
+
   return (
     <div className="App">
       <Loader />
